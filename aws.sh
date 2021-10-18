0ba32044d23cd96601ab5a3fff88f24f3d622f12
Ráfagan Abreu <rafagansa@outlook.com>
2021-10-17 22:28:36 -0300

    Fix submodule

---
+++ aws.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install -y curl apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce docker-compose -y
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+sudo service docker restart
